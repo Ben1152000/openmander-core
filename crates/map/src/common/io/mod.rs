6f38933e27b219c42c76364217ea5a5d4cdbab1f
Ben1152000 <benjamindarnell00@gmail.com>
2025-09-10 17:14:56 -0500

    Split up common.rs into multiple files

---
+++ crates/map/src/common/io/mod.rs
@@ -0,0 +1,9 @@
+mod csr;
+mod csv;
+mod geoparquet;
+mod parquet;
+
+pub use csr::*;
+pub use csv::*;
+pub use geoparquet::*;
+pub use parquet::*;
---
+++ crates/map/src/common/mod.rs
@@ -0,0 +1,7 @@
+mod fs;
+mod geom;
+mod io;
+
+pub use fs::*;
+pub use geom::*;
+pub use io::*;

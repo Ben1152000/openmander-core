09b012c166560a4d6f2374121dccb169e8b095c5
Ben1152000 <benjamindarnell00@gmail.com>
2025-09-12 00:19:40 -0500

    Add pack methods to python bindings

--- cli/src/commands/download.rs
+++ cli/src/commands/download.rs
@@ -1,11 +1,15 @@
 use anyhow::{Ok, Result};
-use openmander::build_pack;
+use openmander::{build_pack, download_pack};
 
 pub fn run(cli: &crate::cli::Cli, args: &crate::cli::DownloadArgs) -> Result<()> {
     let state_code = &args.state.to_ascii_uppercase();
     let out_dir = &args.output.clone().unwrap_or(".".into());
 
-    build_pack(state_code, out_dir, cli.verbose)?;
+    if args.build {
+        build_pack(state_code, &out_dir, cli.verbose)?;
+    } else {
+        download_pack(state_code, &out_dir, cli.verbose)?;
+    }
 
     Ok(())
 }
